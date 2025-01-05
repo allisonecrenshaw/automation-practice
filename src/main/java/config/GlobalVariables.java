9cffc43d1646efe791a6a2fcd0a7ccf06c6e1a1a
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-05 14:11:07 -0600

    Added basic happy path login test.

--- src/main/java/config/GlobalVariables.java
+++ src/main/java/config/GlobalVariables.java
@@ -3,8 +3,10 @@ package config;
 public class GlobalVariables {
   // URLs
   public static final String BASE_URL = "https://www.saucedemo.com";
+  public static final String INVENTORY_URL = "https://www.saucedemo.com/inventory.html";
   
   // User Credentials
   public static final String STANDARD_USER = "standard_user";
-  public static final String PASSWORD = "secret_sauce";
+  public static final String VALID_PASSWORD = "secret_sauce";
+  public static final String INVALID_PASSWORD = "not_secret_sauce";
 }
--- src/test/java/com/example/swaglabsautomation/LoginTest.java
+++ src/test/java/com/example/swaglabsautomation/LoginTest.java
@@ -1,17 +1,27 @@
 package com.example.swaglabsautomation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import config.GlobalVariables;
 
+import com.example.swaglabsautomation.pages.LoginPage;
+
+
 public class LoginTest {
 
   @Test
-  public void successfulLogin() {
+  public void loginWithValidCredentials() {
     WebDriver driver = new ChromeDriver();
 
     driver.get(GlobalVariables.BASE_URL);
+
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login(GlobalVariables.STANDARD_USER, GlobalVariables.VALID_PASSWORD);
+
+    String newUrl = driver.getCurrentUrl();
+    Assertions.assertEquals(GlobalVariables.INVENTORY_URL, newUrl);
 
     driver.quit();
   }

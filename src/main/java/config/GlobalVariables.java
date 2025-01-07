4e52b446450b1f2bbc9da6ac77342e04d89599c0
Allison Crenshaw <allisonecrenshaw@gmail.com>
2025-01-06 19:17:16 -0600

    Added more global vars for WebDriverUniversity site tests.

--- src/main/java/config/GlobalVariables.java
+++ src/main/java/config/GlobalVariables.java
@@ -1,12 +1,16 @@
 package config;
 
 public class GlobalVariables {
-  // URLs
+  // Sauce Demo URLs
   public static final String SAUCE_BASE_URL = "https://www.saucedemo.com/";
   public static final String SAUCE_INVENTORY_URL = "https://www.saucedemo.com/inventory.html";
   
-  // User Credentials
+  // Sauce Demo User Credentials
   public static final String SAUCE_STANDARD_USER = "standard_user";
   public static final String SAUCE_VALID_PASSWORD = "secret_sauce";
   public static final String SAUCE_INVALID_PASSWORD = "not_secret_sauce";
+
+  // Web Driver University URLs
+  public static final String WDU_BASE_URL = "https://webdriveruniversity.com/";
+  public static final String WDU_BUTTONS_URL = "Click-Buttons";
 }
